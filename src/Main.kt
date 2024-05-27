31f005349fe34f61b70d5099bd87dee31a0a371a
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-05-27 10:32:59 +0200

    finished translating "choice_maker.py" into kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,18 @@
+import kotlin.random.Random
+
+
 fun main() {
+    println(compareAll(getOptions()))
+}
 
-    println(compare_all(get_options()))
 
-    fun get_options(): List<String> {
+    fun getOptions(): List<String> {
         println("Insert your options. \nType 'done' when you are finished:")
         val options = mutableListOf<String>()
 
         // Gets all options from the user
+        println("Insert an option:")
         while(true) {
-            println("Insert an option:")
             val option = readlnOrNull()
             if (option?.lowercase() == "done") {
                 break
@@ -22,18 +26,19 @@ fun main() {
 
 
     // Takes a list of options and returns one option
-    fun compare_all(options: List<String>) {
-        while (options.size > 1) {
-            val winner = compare_pair(options.pop(0), options.pop(0))
-            options.append(winner)
+    fun compareAll(options: List<String>): String {
+        val mutableOptions = options.toMutableList()
+
+        while (mutableOptions.size > 1) {
+            val winner = comparePair(mutableOptions.removeAt(0), mutableOptions.removeAt(0))
+            mutableOptions.add(winner)
         }
 
-        return options[0]
+        return mutableOptions[0]
     }
 
 
     // Returns one of the two arguments
-    fun compare_pair(option1: String, option2: String) {
-        return option1 if random.random() < 0.5 else option2
+    fun comparePair(option1: String, option2: String): String {
+        return if (Random.nextFloat() < 0.5) option1 else option2
     }
-}
